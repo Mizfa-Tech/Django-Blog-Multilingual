7a2ee53295c3e6dc93dad71004afe3e536fcb79b
mohammadrezasolo69 <mohammadrezasolo69@gmail.com>
2022-10-30 15:48:17 +0330

    fix bug for translation.py category

--- blog/translation.py
+++ blog/translation.py
@@ -2,19 +2,11 @@ from modeltranslation.translator import register, TranslationOptions
 from blog.models import Post, Category
 
 
-@register(Post)
-class PostTranslationOptions(TranslationOptions):
-    fields = (
-        'meta_title', 'meta_description', 'created_at', 'updated_at',
-        'created_at_jalali', 'updated_at_jalali', "title", "content",
-        "slug", "author", "thumbnail", "thumbnail_alt", 'status', 'category',
-    )
-
-
 @register(Category)
 class CategoryTranslationOptions(TranslationOptions):
-    fields = (
-        'meta_title', 'meta_description', 'created_at', 'updated_at',
-        'created_at_jalali', 'updated_at_jalali', "title", "description",
-        "slug", "thumbnail", "thumbnail_alt", 'status', 'parent_category'
-    )
+    fields = ("title", "description", 'parent_category')
+
+
+@register(Post)
+class PostTranslationOptions(TranslationOptions):
+    fields = ("title", "content", 'category')
